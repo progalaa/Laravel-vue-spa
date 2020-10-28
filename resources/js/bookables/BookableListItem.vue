d8beaee853ec6a90c7fc04635293bc23b1763002
progalaa <alaa.abdelmonem@areebgroup.com>
2020-10-28 10:21:35 +0200

    bookable models and lifecycle

---
+++ resources/js/bookables/BookableListItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <p>{{ content }}  - {{ price }} $ </p>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    props: {"title": String, "content": String, "price": Number},
+}
+</script>
---
+++ resources/js/bookables/Bookables.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <bookable-list-item title="Cheap villa" content="A very cheap villa" :price="1000"></bookable-list-item>
+        <bookable-list-item title="Cheap villa 2" content="A very cheap villa 2" :price="255"></bookable-list-item>
+        <bookable-list-item title="Cheap villa 3" content="A very cheap villa 3" :price="500"></bookable-list-item>
+
+    </div>
+</template>
+
+
+<script>
+import BookableListItem from "./BookableListItem";
+export default {
+    components: {
+        BookableListItem
+    },
+    // beforeCreate() {
+    //     console.log("beforeCreate");
+    // },
+    created() {
+        console.log("created");
+    },
+    // beforeMount() {
+    //     console.log("beforeMount");
+    // },
+    // mounted() {
+    //     console.log("mounted");
+    // },
+    // beforeDestroy() {
+    //     console.log("beforeDestroy");
+    // },
+    // destroyed() {
+    //     console.log("destroyed");
+    // }
+}
+</script>
